d68923293d470127255167c3eb7df6c090337046
GerardFerrerBirbe <gferrerbirbe@gmail.com>
2020-07-24 13:24:36 +0200

    Resources - TeachingMaterials and Exercices Controllers (and Models related)

---
+++ Models/Exercise.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LMSStudent.Models
+{
+    public class Exercise
+    {
+        public int Id { get; set; }        
+        public int AvailableTime { get; set; }
+
+        public virtual Resource Resource { get; set; }
+    }
+}
---
+++ Models/TeachingMaterial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LMSStudent.Models
+{
+    public class TeachingMaterial
+    {
+        public int Id { get; set; }        
+        public string Type { get; set; }
+        public string Link { get; set; }
+
+        public virtual Resource Resource { get; set; }
+
+    }
+}
