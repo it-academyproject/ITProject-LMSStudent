f43b0a8b764f888d9cfd826d746af6c81d3ab0d4
francisco-barcelona <rfrancisco31@hotmail.com>
2020-07-27 18:07:41 +0200

    Users Model

---
+++ Models/Users.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LMSStudent.Models
+{
+    public class Users
+    {
+        [Key]
+        public long IdUSer { get; set; }
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string email { get; set; }
+        public double type { get; set; }
+        public long idItinerary { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,7 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-
+using Microsoft.EntityFrameworkCore;
+using LMSStudent.Models;
 namespace LMSStudent
 {
     public class Startup
@@ -25,6 +26,8 @@ namespace LMSStudent
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<UsersContext>(opt =>
+               opt.UseInMemoryDatabase("UsersList"));
             services.AddControllers();
         }
 
