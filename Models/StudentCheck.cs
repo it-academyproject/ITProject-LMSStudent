d5970177d3600e06c3e66c34294ba50a3eed77dc
GerardFerrerBirbe <gferrerbirbe@gmail.com>
2020-07-28 17:22:05 +0200

    Resources EndPoints Implementation

---
+++ Models/StudentCheck.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LMSStudent.Models
+{
+    public class StudentCheck
+    {
+        public int Id { get; set; }
+        public int ResourceId { get; set; }
+        public int UserId { get; set; }
+        public string File { get; set; }
+        public string Status { get; set; }
+
+        public Resource Resource { get; set; }
+
+        //public User User { get; set; }
+    }
+}
