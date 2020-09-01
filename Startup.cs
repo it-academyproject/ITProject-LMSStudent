5c573bc3502fd8458eeeb4b5a3ce375c37e8165b
CloudSalander <ruben.as532@gmail.com>
2020-09-01 14:03:11 +0200

    Merge fixes

--- Startup.cs
+++ Startup.cs
@@ -30,6 +30,7 @@ namespace LMSStudent
         {
 
             services.AddControllersWithViews();
+
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
